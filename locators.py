2e41696f1299c44c08031a64f40530b9813715b4
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2025-04-23 22:30:03 +0530

    Update locators.py

--- locators.py
+++ locators.py
@@ -13,8 +13,8 @@ class OrangeHRMLocators:
     dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
 
     # PIM Page Locators
-    pim_menu = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='PIM']") #//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='PIM']
-    add_employee_button = (By.XPATH, "//a[@class='oxd-topbar-body-nav-tab-item' and text()='Add Employee']") #//a[@class='oxd-topbar-body-nav-tab-item' and text()='Add Employee']
+    pim_menu = (By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='PIM']") 
+    add_employee_button = (By.XPATH, "//a[@class='oxd-topbar-body-nav-tab-item' and text()='Add Employee']") 
     first_name_input = (By.XPATH, "//input[@placeholder='First Name']") #//input[@placeholder='First Name']
     last_name_input = (By.XPATH, "//input[@placeholder='Last Name']")   #//input[@placeholder='Last Name']
     save_button = (By.XPATH, "//button[@type='submit']")    #//button[@type='submit']
