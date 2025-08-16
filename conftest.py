e1d70ef821a948f247b5f613a9fe82d11f9c3402
David Selvaraj <dsraj03@gmail.com>
2025-08-16 21:10:35 +0530

    conftest file added

---
+++ conftest.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def driver():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    return driver
+
+
+
+
